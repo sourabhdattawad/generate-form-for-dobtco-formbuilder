8ed373695bc93ef656eb4a048cb4e1a5f58caf39
sourabhdattawad <sourabhdattawad@gmail.com>
2016-02-26 02:12:23 +0530

    minor changes

--- generator.js
+++ generator.js
@@ -1,12 +1,10 @@
-function dynamicUIGenerator(fieldString) {
+function generateUI(fieldString) {
     var output = "";
     var field = JSON.parse(fieldString).fields;
     for (key in field) {
-        console.log("inside for loop" + key);
-        console.log(field[key].field_type);
-        output += "<div class='form-group'>";
-        output += "<label class='col-md-4 control-label' >" + field[key].label + "</label> ";
-        output += "<div class='col-md-8'>";
+
+        output+='<div>';
+        output += "<label>" + field[key].label + "</label> ";
         switch (field[key].field_type) {
             case "text":
             case "date":
@@ -14,16 +12,16 @@ function dynamicUIGenerator(fieldString) {
             case "number":
             case "website":
             case "email":
-                var commonField = '<input type=' + '"' + field[key].field_type + '"' + ' name = ' + '"' + field[key].label + '"' + ' id=' + '"' + field[key].label + '"' + 'placeholder ="' + field[key].label + '" required=""/>';
+                var commonField = '<input type=' + '"' + field[key].field_type + '"' + ' name = ' + '"' + field[key].label + '"' + ' id=' + '"' + field[key].label + '"' + 'placeholder ="' + field[key].label + '" required/>';
                 output += commonField;
                 break;
             case "paragraph":
-                var paraGraphField = '<textarea class="form-control" rows="3"  name =' + '"' + field[key].label + '"' + ' id=' + '"' + field[key].label + '"' + ' required="">' + '</textarea>'
+                var paraGraphField = '<textarea rows="3"  name =' + '"' + field[key].label + '"' + ' id=' + '"' + field[key].label + '"' + ' required>' + '</textarea>'
                 output += paraGraphField;
                 break;
             case "dropdown":
                 var dropdownField = '';
-                dropdownField += '<select name =' + '"' + field[key].label + '"' + ' required="">';
+                dropdownField += '<select name =' + '"' + field[key].label + '"' + ' required>';
                 for (var i = 0; i < field[key].field_options.options.length; i++) {
 
                     dropdownField += '<option name=' + '"' + field[key].field_options.options[i].label + '"' + 'value =' + '"' + field[key].field_options.options[i].label + '"' + '>' + field[key].field_options.options[i].label + '</option>';
@@ -48,19 +46,18 @@ function dynamicUIGenerator(fieldString) {
                 output += radioField;
                 break;
             case "price":
-                output += '<input type= "number"' + ' name = ' + '"' + field[key].label + '"' + ' id=' + '"' + field[key].label + '"' + ' value="" required=""/>'
+                output += '<input type= "number"' + ' name = ' + '"' + field[key].label + '"' + ' id=' + '"' + field[key].label + '"' + ' required/>'
                 break;
             case "address":
                 var address = '';
-                address += '<label>Address</label><input type="text" name ="address" placeholder="Address" required="">';
-                address += '<label>City</label><input type="text" name ="city" placeholder="City" required="">';
-                address += '<label>State</label><input type="text" name ="state" placeholder="State" required="">';
-                address += '<label>Zipcode</label><input type="text" name ="zipcode" placeholder="Zipcode" required="">';
+                address += '<label>Address</label><input type="text" name ="address" placeholder="Address" required>';
+                address += '<label>City</label><input type="text" name ="city" placeholder="City" required>';
+                address += '<label>State</label><input type="text" name ="state" placeholder="State" required>';
+                address += '<label>Zipcode</label><input type="text" name ="zipcode" placeholder="Zipcode" required>';
 
                 output += address;
                 break;
         }
-        output += "</div>";
         output += "</div>";
 
     }
